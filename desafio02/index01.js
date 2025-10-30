2dc95a95ea0d27dec27d621debe3fcf0834af350
vanessaugioni <vanessaugionicontato@gmail.com>
2025-10-30 19:30:48 -0300

    index01

---
+++ desafio02/index01.js
@@ -0,0 +1,38 @@
+class LegacyPaymentSystem {
+  makePayment(amount) {
+    console.log(`Pagando R$${amount} com sistema legado.`);
+  }
+}
+
+class ModernPaymentAPI {
+  process(amountInCents) {
+    console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+  }
+}
+
+class Paydapter extends LegacyPaymentSystem {
+  constructor(payService) {
+    super();
+    this.payService = payService;
+  }
+
+  send(amount) {
+    this.payService.process(amount);
+  }
+}
+
+
+function typePay(legacy, modern) {
+  legacy.send(modern);
+}
+
+
+const payType = new LegacyPaymentSystem();
+notifyUser(payType, "teste"); 
+
+
+const payService = new ModernPaymentAPI();
+const payMod = new Paydapter(payService);
+
+
+notifyUser(payMod, "teste saída"); 
